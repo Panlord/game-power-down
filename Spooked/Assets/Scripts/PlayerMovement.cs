0e4eb3d40ebf813881bc23714a4b93f643b94aec
Panlord <aaronthepan@gmail.com>
2021-05-27 02:08:11 -0700

    Added gravity

--- Spooked/Assets/Scripts/PlayerMovement.cs
+++ Spooked/Assets/Scripts/PlayerMovement.cs
@@ -10,9 +10,24 @@ public class PlayerMovement : MonoBehaviour
 
     // Player character's movement speed.
     [SerializeField] private float Speed;
+    public float Gravity = -9.81f;
+    private Vector3 Velocity;
+
+    // Checking if the player is touching the ground.
+    [SerializeField] private Transform GroundCheck;
+    [SerializeField] private float GroundDistance = 0.4f;
+    [SerializeField] private LayerMask GroundMask;
+    private bool IsOnGround;
 
     void Update()
     {
+        // Check if the player is on the ground.
+        this.IsOnGround = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
+        if (IsOnGround && Velocity.y < 0)
+        {
+            Velocity.y = -2f;
+        }
+
         // Move the player based on what buttons were pressed.
         // W and S add 1 and -1 to the Vertical respectively.
         // A and D add -1 and 1 to the Horizontal respectively.
@@ -21,5 +36,9 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         Controller.Move(move * Speed * Time.deltaTime);
+
+        // Apply gravity.
+        this.Velocity.y += Gravity * Time.deltaTime;
+        Controller.Move(Velocity * Time.deltaTime);
     }
 }
