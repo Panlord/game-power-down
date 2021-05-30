b013f012c059e5cff6355cbd0e3e4a1bf9df7dae
palenciaVik <palenciavik@gmail.com>
2021-05-29 19:50:38 -0700

    added music and script to change the audioclip being played

--- Spooked/Assets/Scripts/MouseLook.cs
+++ Spooked/Assets/Scripts/MouseLook.cs
@@ -24,7 +24,7 @@ public class MouseLook : MonoBehaviour
 
     void Update()
     {
-        
+        // check if gamepad is connected
         if(gamepad != null){
          // When the mouse moves side to side, the camera (and character) move side to side.
         float moveX = Gamepad.current.rightStick.x.ReadValue() * MouseSensitivity * Time.deltaTime;
@@ -39,15 +39,12 @@ public class MouseLook : MonoBehaviour
         PlayerBody.Rotate(Vector3.up * moveX);
         }
         
+        // same as above but for mouse input
         float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
-        // Same for when the mouse moves up and down.
         float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
-        // Stop the player from being an owl and rotating their head/camera 180+ degrees,
-        // i.e. clamp the camera.
         this.XRotation -= mouseY;
         this.XRotation = Mathf.Clamp(XRotation, -90f, 90f);
         transform.localRotation = Quaternion.Euler(XRotation, 0f, 0f);
-        // Rotate the player from side-to-side when moving the camera side-to-side.
         PlayerBody.Rotate(Vector3.up * mouseX);
 
     }
