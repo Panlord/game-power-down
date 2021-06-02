336de4ab2ef367bb6dd0e580e420436b3be498d2
Panlord <aaronthepan@gmail.com>
2021-06-01 23:03:32 -0700

    Got single doors working for small rooms

---
+++ Spooked/Assets/Scripts/DoorController.cs
@@ -0,0 +1,29 @@
+// Created by Aaron Pan, based off of SpeedTutor's "OPENING a DOOR in UNITY with a RAYCAST" tutorial on YouTube.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    private Animator DoorAnimator;
+    private bool DoorOpen = false;
+    private void Awake()
+    {
+        DoorAnimator = gameObject.GetComponent<Animator>();
+    }
+
+    public void PlayAnimation()
+    {
+        if (!DoorOpen)
+        {
+            DoorAnimator.Play("DoorOpen", 0, 0.0f);
+            DoorOpen = true;
+        }
+        else
+        {
+            DoorAnimator.Play("DoorClose", 0, 0.0f);
+            DoorOpen = false;
+        }
+    }
+}
---
+++ Spooked/Assets/Scripts/DoubleDoorController.cs
@@ -0,0 +1,29 @@
+// Created by Aaron Pan, based off of SpeedTutor's "OPENING a DOOR in UNITY with a RAYCAST" tutorial on YouTube.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleDoorController : MonoBehaviour
+{
+    private Animator DoubleDoorAnimator;
+    private bool DoubleDoorOpen = false;
+    private void Awake()
+    {
+        DoubleDoorAnimator = gameObject.GetComponent<Animator>();
+    }
+
+    public void PlayAnimation()
+    {
+        if (!DoubleDoorOpen)
+        {
+            DoubleDoorAnimator.Play("DoubleDoorOpen", 0, 0.0f);
+            DoubleDoorOpen = true;
+        }
+        else
+        {
+            DoubleDoorAnimator.Play("DoubleDoorClose", 0, 0.0f);
+            DoubleDoorOpen = false;
+        }
+    }
+}
