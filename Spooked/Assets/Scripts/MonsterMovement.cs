1a50a03bb1a96f41f432918fec2466fd4d4947c9
sarah <45083409+imsarra@users.noreply.github.com>
2021-06-02 21:19:45 +0300

    Update MonsterMovement.cs

--- Spooked/Assets/Scripts/MonsterMovement.cs
+++ Spooked/Assets/Scripts/MonsterMovement.cs
@@ -1,39 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MonsterMovement : MonoBehaviour
 {
     [SerializeField] private GameObject monster;
     [SerializeField] private GameObject player;
-    private float LerpDuration = 0.01f;
-
-    private Vector3 MovementDirection;
     private float counter = 0f;
+    private float time = 5f;
+    private float range = 20f;
 
+    public Transform target;
+    public NavMeshAgent enemy;
+
+    private string[] rooms = {"Conference Room A", "Conference Room A (1)", "Conference Room A (2)", "Conference Room A (3)",
+                                "Conference Room A (4)", "Conference Room A (5)", "Conference Room B", "Conference Room B (1)", 
+                                "Conference Room B (2)", "Small Office Room A", "Small Office Room A (1)", "Small Office Room A (2)",
+                                "Small Office Room A (3)", "Small Office Room A (4)", "Small Office Room A (5)", "Small Office Room A (6)",
+                                "Small Office Room A (7)", "Small Office Room B", "Small Office Room B (1)", "Small Office Room B (2)", 
+                                "Small Office Room B (3)", "Small Office Room B (4)", "Small Office Room B (5)", "Small Office Room B (6)",
+                                "Small Office Room B (7)", "Small Office Room C", "Small Office Room C (1)", "Small Office Room C (2)","Small Office Room C (3)",
+                                "Small Office Room C (4)", "Small Office Room C (5)", "Small Office Room C (6)", "Small Office Room D (1)", "Small Office Room D (2)",
+                                "Small Office Room D (3)", "Boys Restroom", "Boys Restroom (1)", "Girls Restroom", "Girls Restroom (1)", "Small Office Room D",
+                                "Computer Room", "Computer Room (1)", "Computer Room (2)", "Computer Room (3)", "Computer Room (4)"};
+    
     void Update()
     {
-        this.MovementDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
-        var playerPosition = this.player.transform.position;
-        var monsterPosition = this.monster.transform.position;
+        
+        //enemy.SetDestination(target.position);
 
-        // if the player stops moving, monster lerps to its direction
-        if(this.MovementDirection.Equals(Vector3.zero))
+        if(counter>=time)
         {
-            monsterPosition = Vector3.Lerp(monsterPosition, new Vector3(playerPosition.x, playerPosition.y, playerPosition.z), LerpDuration);
-            this.monster.transform.position = monsterPosition;
-            this.counter=0;
+
+            if(Vector3.Distance(player.transform.position, monster.transform.position)<=range)
+            {
+                Debug.Log("monster nearby");
+                enemy.SetDestination(target.position);
+            }
+           
+            if(Vector3.Distance(player.transform.position, monster.transform.position)>range)
+            {
+                int roomOrFloor = Random.Range(1,3); 
+                if(roomOrFloor == 1)
+                {
+                    int numOfFloor = Random.Range(7,128);
+                    string floor = "Floor_01_Snaps014 ("+numOfFloor.ToString()+")";
+                    this.monster.transform.position = new Vector3(GameObject.Find(floor).transform.position.x, this.monster.transform.position.y, GameObject.Find(floor).transform.position.z);
+                }
+                else
+                {
+                    int numOfRoom = Random.Range(0, rooms.Length);
+                    this.monster.transform.position = new Vector3(GameObject.Find(rooms[numOfRoom]).transform.position.x, this.monster.transform.position.y, GameObject.Find(rooms[numOfRoom]).transform.position.z);
+                }
+                counter = 0;
+            }
+            
         }
 
-        // if the player is currently moving, start lerping after some time (0.3 seconds)
         else
         {
-            this.counter+=Time.deltaTime;
-            if(this.counter>=0.3f)
-            {
-                monsterPosition = Vector3.Lerp(monsterPosition, new Vector3(playerPosition.x, playerPosition.y, playerPosition.z), LerpDuration);
-                this.monster.transform.position = monsterPosition;
-            } 
+            counter+=Time.deltaTime;
         }
+
     }
 }
