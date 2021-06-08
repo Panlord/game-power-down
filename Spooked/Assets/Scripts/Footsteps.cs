c76f18066d5dcf5fc417bed76d41ebeac95d9c49
palenciaVik <palenciavik@gmail.com>
2021-06-07 23:49:43 -0700

    added footsteps.cs

---
+++ Spooked/Assets/Scripts/Footsteps.cs
@@ -0,0 +1,27 @@
+// writted by victor, used https://www.youtube.com/watch?v=ih8gyGeC7xs&t=320s as a source. 
+using UnityEngine;
+using System.Collections;
+
+public class Footsteps : MonoBehaviour 
+{
+
+    // Use this for initialization
+    CharacterController Player;
+    private AudioSource Audio;
+    void Start () 
+    {
+            Player = GetComponent<CharacterController>();
+            Audio = GetComponent<AudioSource>();
+    }
+ 
+ // Update is called once per frame
+        void Update () {
+        if (Player.isGrounded == true && (Player.velocity.magnitude > 3f) && Audio.isPlaying == false)
+        {  
+            Audio.volume = Random.Range(0.3f, 0.5f);
+            Audio.pitch = Random.Range(0.8f, 1.1f);
+            Audio.Play();
+        }
+ }
+}
+
