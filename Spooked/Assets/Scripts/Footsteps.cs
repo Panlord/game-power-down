cc02542d51ae6a0750623972bc3853b23a49764b
palenciaVik <palenciavik@gmail.com>
2021-06-08 00:00:09 -0700

    minor update

--- Spooked/Assets/Scripts/Footsteps.cs
+++ Spooked/Assets/Scripts/Footsteps.cs
@@ -4,23 +4,24 @@ using System.Collections;
 
 public class Footsteps : MonoBehaviour 
 {
-
-    // Use this for initialization
     CharacterController Player;
     private AudioSource Audio;
+
     void Start () 
     {
-            Player = GetComponent<CharacterController>();
-            Audio = GetComponent<AudioSource>();
+        Player = GetComponent<CharacterController>();
+        Audio = GetComponent<AudioSource>();
     }
  
- // Update is called once per frame
-        void Update () {
-        if (Player.isGrounded == true && (Player.velocity.magnitude > 3f) && Audio.isPlaying == false)
-        {  
+
+        void Update () 
+        {   
+            // check if player is on ground and moving 
+            if (Player.isGrounded == true && (Player.velocity.magnitude > 3f) && Audio.isPlaying == false)
+            {  
             Audio.volume = Random.Range(0.3f, 0.45f);
             Audio.pitch = Random.Range(0.8f, 1.1f);
             Audio.Play();
+            }
         }
- }
 }
