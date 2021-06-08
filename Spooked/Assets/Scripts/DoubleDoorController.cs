b894422c8418ed4965ffe7f11b4dd262dae4ae77
palenciaVik <palenciavik@gmail.com>
2021-06-07 22:12:01 -0700

    update DoubleDoorController and added sound to double doors

--- Spooked/Assets/Scripts/DoubleDoorController.cs
+++ Spooked/Assets/Scripts/DoubleDoorController.cs
@@ -8,10 +8,16 @@ public class DoubleDoorController : MonoBehaviour
 {
     private Animator DoubleDoorAnimator;
     private bool DoubleDoorOpen = false;
+    private AudioSource Audio;
+    [SerializeField]
+    public AudioClip open;
+    [SerializeField]
+    private AudioClip close;
 
     private void Awake()
     {
         DoubleDoorAnimator = gameObject.GetComponent<Animator>();
+        Audio = GetComponent<AudioSource>();
     }
 
     public void PlayAnimation()
@@ -20,11 +26,21 @@ public class DoubleDoorController : MonoBehaviour
         {
             DoubleDoorAnimator.Play("DoubleDoorOpen", 0, 0.0f);
             DoubleDoorOpen = true;
+            if (Audio != null)
+            {
+                Audio.clip = open;
+                Audio.Play();
+            }
         }
         else
         {
             DoubleDoorAnimator.Play("DoubleDoorClose", 0, 0.0f);
             DoubleDoorOpen = false;
+            if (Audio != null)
+            {
+                Audio.clip = close;
+                Audio.Play();
+            }
         }
     }
 
